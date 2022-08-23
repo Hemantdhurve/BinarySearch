ac4be3a3334b86c4b817f091e12114bc7bb3017e
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-24 00:18:57 +0530

    Welcome Message

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+        }
+    }
+}
