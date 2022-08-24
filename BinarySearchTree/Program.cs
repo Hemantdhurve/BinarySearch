d3ef9560e42579f7882b8c1b7a745f5b9c7f1e8d
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-24 16:03:59 +0530

    [Hemant] Added UC1 Created Binary Search Tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,32 @@ namespace BinarySearchTree
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree Program");
+
+            bool check = true;
+            
+
+            Console.WriteLine("Select Options :\n" + 
+                "1)Enter elements in the Binary Search Tree\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    BinarySearch<int> binary = new BinarySearch<int>(56);        //56 taken as a root element
+                    binary.Add(30);
+                    binary.Add(70);
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+                    Console.Write("Root Node is : ");
+                    binary.Display();
+                    break;
+
+                default:
+
+                    Console.WriteLine("Please enter Proper option");
+                    break;
+            }
         }
     }
 }
